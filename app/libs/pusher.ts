b7db12dd1f5b1fa7f66ea0de8feca2ac7d91c758
simpleogdev <mrzkk07@gmail.com>
2023-06-28 09:37:10 +0530

    first commit

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -1,5 +1,27 @@
-import PusherServer from 'pusher'
-import PusherClient from 'pusher-js'
+// import PusherServer from 'pusher'
+// import PusherClient from 'pusher-js'
+
+// export const pusherServer = new PusherServer({
+//   appId: process.env.PUSHER_APP_ID!,
+//   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+//   secret: process.env.PUSHER_SECRET!,
+//   cluster: 'ap',
+//   useTLS: true,
+// });
+
+// export const pusherClient = new PusherClient(
+//   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+//   {
+//     channelAuthorization: {
+//       endpoint: '/api/pusher/auth',
+//       transport: 'ajax',
+//     },
+//     cluster: 'ap',
+//   }
+// );
+
+import Pusher from 'pusher-js';
+import PusherServer from 'pusher';
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
@@ -9,13 +31,8 @@ export const pusherServer = new PusherServer({
   useTLS: true,
 });
 
-export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  {
-    channelAuthorization: {
-      endpoint: '/api/pusher/auth',
-      transport: 'ajax',
-    },
-    cluster: 'eu',
-  }
-);
+export const pusherClient = new Pusher(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+  authEndpoint: '/api/pusher/auth',
+  cluster: 'ap2',
+  forceTLS: true,
+});
